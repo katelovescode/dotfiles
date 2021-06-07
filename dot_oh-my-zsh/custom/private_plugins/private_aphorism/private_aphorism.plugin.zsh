6da5809c95b9bcf89454f4fd34e18c532dc78aff
Kate Donaldson <kate@katelovescode.com>
2021-06-07 15:41:31 -0500

    Temporarily remove aphorism/lolcat call

--- dot_oh-my-zsh/custom/private_plugins/private_aphorism/private_aphorism.plugin.zsh
+++ dot_oh-my-zsh/custom/private_plugins/private_aphorism/private_aphorism.plugin.zsh
@@ -3,18 +3,18 @@
 
 echo "\nGreetings, Sarge\n\n"
 
-if which ruby >/dev/null 2>&1; then
-  source ~/.rvm/scripts/rvm
-  rvm use default >/dev/null 2>&1
-  if [ `gem list '^aphorism$' -i` = true ] && [ `gem list '^lolcat$' -i` = true ]; then
-    aphorism | lolcat
-  else
-    echo "initializing aphorism with lolcat"
-    gem install --user-install bundler aphorism lolcat --silent
-    aphorism | lolcat
-  fi
-else
-  echo "Ruby is required for aphorism to work, please install ruby and try again"
-fi
+# if which ruby >/dev/null 2>&1; then
+#   source ~/.rvm/scripts/rvm
+#   rvm use default >/dev/null 2>&1
+#   if [ `gem list '^aphorism$' -i` = true ] && [ `gem list '^lolcat$' -i` = true ]; then
+#     aphorism | lolcat
+#   else
+#     echo "initializing aphorism with lolcat"
+#     gem install --user-install bundler aphorism lolcat --silent
+#     aphorism | lolcat
+#   fi
+# else
+#   echo "Ruby is required for aphorism to work, please install ruby and try again"
+# fi
 
 echo "\n"
