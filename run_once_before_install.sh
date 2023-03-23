38e4c82d3e67bcda43daa9fb2e38da272596f889
Kate Donaldson <kate@katelovescode.com>
2023-03-22 20:17:37 -0500

    Fixes to github protocol and splitting of scripts

--- run_once_before_install.sh
+++ run_once_before_install.sh
@@ -45,7 +45,7 @@ brew update
 
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
-brew bundle
+brew bundle --file /Users/katedonaldson/.local/share/chezmoi/Brewfile
 
 # Configure Chrome to start where you left off on startup
 cd $HOME/Library/Application\ Support/Google/Chrome/Default
@@ -76,24 +76,3 @@ fi
 
 pg_ctl -D /usr/local/var/postgres start > /dev/null 2>&1
 brew services start postgresql > /dev/null 2>&1
-
-# Set macOS preferences
-cd $HOME
-source $HOME/.macos
-
-# needed for Docker and other Intel apps on M1 chip
-if [[ $(uname -p) == 'arm' ]]; then
-  echo "Running on M1 chip"
-  softwareupdate -lia --install-rosetta --agree-to-license
-else
-  echo "Running on Darwin chip"
-  softwareupdate -lia
-fi
-
-# restart
-echo "\n\nDo you want to restart now?  Anything but 'y' will prevent restarting.\n"
-if read -q "shutdownpref?"; then
-  sudo shutdown -r now
-else
-  echo "\n\nskipping restart, if you see weird behavior you might need to restart manually"
-fi
